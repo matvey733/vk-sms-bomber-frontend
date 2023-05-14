6a39a6293d6bd57d60699684b9a607c1388d5dd7
Matvey <morozovmatvej75@gmail.com>
2023-05-14 17:52:59 +0300

    add plain html javascript frontend

--- client/main.js
+++ client/main.js
@@ -1,5 +1,17 @@
 const btn = document.querySelector(".bomb-btn");
+const resultDiv = document.querySelector(".result");
+const progressDiv = document.querySelector(".progress");
 
-btn.addEventListener("click", () => {
-  const promises = 
+btn.addEventListener("click", async () => {
+  progressDiv.textContent = "Bombing...";
+
+  try {
+    const result = await fetch("https://vk-sms-bomber-8.onrender.com/bomb", {
+    });
+    console.log(result);
+    resultDiv.textContent = result.status;
+    progressDiv.textContent = "Done!";
+  } catch (err) {
+    console.error(err);
+  }
 })
